550424ae22e9696faf4172832524b2855ccbb32f
Unknown <randy.maile@sharp.com>
2020-11-19 07:48:01 -0800

    asdf

--- components/nav.js
+++ components/nav.js
@@ -5,13 +5,14 @@ const Nav = ({ siteTitle }) => (
   <nav className='z-10 fixed w-full p-4 flex bg-tint h-12'>
     <div className='flex items-center flex-shrink-0 mr-6'>
       <Link href='/'>
-        <a><Image
-          src='/logo.png'
-          alt='Sharp Logo'
-          width={110}
-          height={35}
-          layout='intrinsic'
-           />
+        <a>
+          <Image
+            src='/logo.png'
+            alt='Sharp Logo'
+            width={110}
+            height={35}
+            layout='intrinsic'
+          />
         </a>
       </Link>
     </div>
